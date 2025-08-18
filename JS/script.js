9d61955171419f48747e4ab13bc67a65d3f617a3
Jaser Ramirez <JArody07@Jasers-MBP-2.lan>
2025-08-17 22:23:03 -0700

    Final touches on the website, just need to add details to projects and deployment

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,22 @@
 document.getElementById('show-all-projects').addEventListener('click', function(e) {
     e.preventDefault();
-    var hidden = document.querySelectorAll('.hidden-project');
-    hidden.forEach(function(card) {
+    var hiddenCards = document.querySelectorAll('.hidden-project');
+    hiddenCards.forEach(function(card) {
         card.style.display = 'flex';
-        card.classList.remove('hidden-project');
         card.classList.add('revealed');
     });
     this.style.display = 'none';
+    document.getElementById('show-less-projects').style.display = 'inline-block';
+});
+
+document.getElementById('show-less-projects').addEventListener('click', function(e) {
+    e.preventDefault();
+    var hiddenCards = document.querySelectorAll('.revealed');
+    hiddenCards.forEach(function(card) {
+        card.style.display = 'none';
+        card.classList.remove('revealed');
+        card.classList.add('hidden-project');
+    });
+    this.style.display = 'none';
+    document.getElementById('show-all-projects').style.display = 'inline-block';
 });
